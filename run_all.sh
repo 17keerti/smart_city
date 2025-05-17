f1249f3b11e5431307630d2c9c39653165814d62
17keerti <17keerti@gmail.com>
2025-05-17 09:19:42 -0700

    update run_all.sh to start Kafka and all components

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Start Kafka and Zookeeper containers in detached mode
+echo "Starting Kafka and Zookeeper with docker-compose..."
+docker-compose up -d
+
+# Wait a bit to make sure Kafka is ready
+echo "Waiting 10 seconds for Kafka to start..."
+sleep 10
+
+# Array of commands to run in new Terminal windows
 commands=(
   "python3 publishers/air_quality_publisher.py"
   "python3 publishers/weather_publisher.py"
@@ -9,6 +18,12 @@ commands=(
   "python3 subscribers/public_interface.py"
 )
 
+
+# Launch each command in a new Terminal window
 for cmd in "${commands[@]}"; do
+  echo "Launching: $cmd"
   osascript -e "tell application \"Terminal\" to do script \"cd $(pwd); $cmd\""
+  sleep 1
 done
+
+echo "All components launched."
