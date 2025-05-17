5f821f49c2e80978b3f7bd5b49f8564a803a0abe
17keerti <17keerti@gmail.com>
2025-05-17 09:07:15 -0700

    add basic environmental monitor subscriber

---
+++ subscribers/environmental_monitor.py
@@ -0,0 +1,22 @@
+from confluent_kafka import Consumer
+import json
+
+consumer = Consumer({
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'env_monitor',
+    'auto.offset.reset': 'earliest'
+})
+
+consumer.subscribe(['air_quality', 'weather'])
+
+print("Environmental Monitoring System is running...")
+while True:
+    msg = consumer.poll(1.0)
+    if msg is None:
+        continue
+    if msg.error():
+        print("Consumer error: {}".format(msg.error()))
+        continue
+
+    data = json.loads(msg.value().decode('utf-8'))
+    print("Received:", data)
